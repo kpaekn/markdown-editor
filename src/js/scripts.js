2720a70cb517699b3ac9dc7adc3a1fcdd9061504
Kevin P <kpaek@hawaii.edu>
2013-08-27 13:19:15 -1000

    added menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,10 +3,32 @@ var preview = $('.markdown-preview');
 // markdown converter
 var converter = new Markdown.Converter();
 
-var fileBrowser = new FileBrowser();
 var codeMirror = CodeMirror(document.body, {
 	mode: 'markdown',
 	theme: 'twilight'
 }).on('change', function(instance, obj) {
 	preview.html(converter.makeHtml(instance.getValue()));
 });
+
+var fileBrowser = new FileBrowser();
+
+var gui = require('nw.gui');
+var menu = new gui.Menu({
+	type: 'menubar'
+});
+
+var fileMenuItem = new gui.MenuItem({
+	label: 'File'
+});
+fileMenuItem.submenu = new gui.Menu();
+fileMenuItem.submenu.append(new gui.MenuItem({ label: 'New' }));
+fileMenuItem.submenu.append(new gui.MenuItem({ label: 'Open' }));
+fileMenuItem.submenu.append(new gui.MenuItem({ label: 'Save' }));
+fileMenuItem.submenu.append(new gui.MenuItem({ label: 'Save as...' }));
+fileMenuItem.submenu.append(new gui.MenuItem({ type: 'separator' }));
+fileMenuItem.submenu.append(new gui.MenuItem({ label: 'Close' }));
+
+menu.append(fileMenuItem);
+
+
+gui.Window.get().menu = menu;
